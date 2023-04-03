0f6e6301f573bb730f0cb9514d06d76a6b3204e3
akhmed <akhmed@storyAkhmedMacBook-Air.local>
2023-04-03 17:04:47 +0300

    1

---
+++ BottleOfWater.java
@@ -0,0 +1,10 @@
+package hw4;
+
+import java.math.BigDecimal;
+
+public class BottleOfWater extends Drinkables {
+
+    public BottleOfWater(String name, BigDecimal price, Double volume) {
+        super(name, price, volume);
+    }
+}
